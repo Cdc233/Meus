de7be47b12fce56dde7b7eb416bbc821e2ea7caa
Cdc233 <cdc12344@hotmail.com>
2021-04-22 12:55:25 -0300

    ex de python

--- Meus exercícios de python/ex005.py
+++ Meus exercícios de python/ex005.py
@@ -1,11 +1,11 @@
 #Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor.
 
 while True:
-     a = int(input('Digite um número: '))
+   a = int(input('Digite um número: '))
 
-     print(f'o antecessor de {a} é {a-1} e o sucessor é {a+1}')
+   print(f'O antecessor de {a} é {a-1} e o sucessor é {a+1}')
 
-     b = str(input('Deseja continuar? [S/N]: ')).upper
-     
-    if b == 'Nn':
-       break 
+   b = input('Deseja continuar?[S/N]: ')
+
+   if b in 'Nn':
+      break
--- Meus exercícios de python/ex006.py
+++ Meus exercícios de python/ex006.py
@@ -1,1 +1,6 @@
 #Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+
+a = int(input('Digite um numero: '))
+
+print(f'O triplo de {a} é {3*a}, seu dobro é {2*a} e sua raiz quadrada é {a**(0.5)}')
